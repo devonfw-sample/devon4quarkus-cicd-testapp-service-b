374667c7a1bfe42aa07ceedc0dec17cf9accfbe8
sujith-mn <sujith.nair@capgemini.com>
2022-02-14 12:38:43 +0530

    Enabled H2 database

---
+++ src/main/resources/db/migration/V0002__R001_Master_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO User(id, modificationCounter, firstname, lastname) VALUES (0, 1,'Hello' ,'World');
+INSERT INTO User(id, modificationCounter, firstname, lastname) VALUES (1, 1, 'Welcome', 'Quarkus' );
+INSERT INTO User(id, modificationCounter, firstname, lastname) VALUES (2, 1, 'Devon', 'Fw');
